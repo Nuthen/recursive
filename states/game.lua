0214e83ccc0b2236a39a873dd2e1c696018ff9d9
Nuthen <nuthen@comcast.net>
2015-02-03 01:33:18 -0500

    modular koch

--- states/game.lua
+++ states/game.lua
@@ -7,8 +7,19 @@ function game:enter()
     --self.shapes = {{x - 200, y - 200, x - 200, y + 200, x + 200, y + 200, x + 200, y - 200}}
 	
 	self.angle = math.rad(60)
-	local dy = math.tan(self.angle)*200
-	self.lines = {{x - 200, y + dy/2, x + 200, y + dy/2}, {x + 200, y + dy/2, x, y - dy/2}, {x, y - dy/2, x - 200, y + dy/2}}
+	--local dy = math.tan(self.angle)*200
+	--self.lines = {{x - 200, y + dy/2, x + 200, y + dy/2}, {x + 200, y + dy/2, x, y - dy/2}, {x, y - dy/2, x - 200, y + dy/2}}
+	--self.lines = {{x - 200, y - 200, x - 200, y + 200}, {x - 200, y + 200, x + 200, y + 200}, {x + 200, y + 200, x + 200, y - 200}, {x + 200, y - 200, x - 200, y - 200}}
+	
+	self.lines = {}
+	
+	self.sides = 3
+	self.points = {}
+	
+	self.r = 200
+	
+	self:start()
+	
 	
 	self.steps = 0
 	
@@ -19,6 +30,28 @@ function game:enter()
 	--self.canvas:renderTo(function()
 	--	love.graphics.polygon('line', self.shapes[1])
 	--end)
+end
+
+function game:start()
+	local x, y = love.graphics.getWidth()/2, love.graphics.getHeight()/2
+
+	self.lines = {}
+	
+	--self.sides = 3
+	self.points = {}
+	
+	self.r = 200
+
+	for i = 1, self.sides do
+		local angle = math.rad((360/self.sides)*i)
+		local x1, y1 = x + math.cos(angle)*self.r, y + math.sin(angle)*self.r
+		table.insert(self.points, {x1, y1})
+	end
+	
+	for i = 1, #self.points-1 do
+		table.insert(self.lines, {self.points[i][1], self.points[i][2], self.points[i+1][1], self.points[i+1][2]})
+	end
+	table.insert(self.lines, {self.points[#self.points][1], self.points[#self.points][2], self.points[1][1], self.points[1][2]})
 end
 
 function game:update(dt)
@@ -46,7 +79,7 @@ function game:keypressed(key, isrepeat)
 			local angle = math.angle(x1, y1, x2, y2)
 			local dist = math.dist(p1x, p1y, p2x, p2y)
 			
-			local p3x, p3y = p1x + math.cos(angle+self.angle)*dist, p1y + math.sin(angle+self.angle)*dist
+			local p3x, p3y = p1x + math.cos(angle-self.angle)*dist, p1y + math.sin(angle-self.angle)*dist
 			
 			table.remove(self.lines, i)
 			
@@ -58,12 +91,21 @@ function game:keypressed(key, isrepeat)
 			table.insert(self.lines, {p3x, p3y, p2x, p2y})
 		end
 	end
+	
+	if tonumber(key) and tonumber(key) > 2 and tonumber(key) < 10 then
+		self.sides = tonumber(key)
+		
+		self:start()
+	end
 end
 
 function game:mousepressed(x, y, mbutton)
     if console.mousepressed(x, y, mbutton) then
         return
     end
+	
+	if mbutton == 'wu' then self.angle = self.angle + math.rad(3) end
+	if mbutton == 'wd' then self.angle = self.angle - math.rad(3) end
 end
 
 function game:draw()
@@ -73,4 +115,8 @@ function game:draw()
 	
 	--love.graphics.draw(self.canvas)
 	love.graphics.print(love.timer.getFPS(), 5, 5)
+	love.graphics.print('angle: '..math.deg(self.angle)..' degrees', 5, 35)
+	love.graphics.print('Press "space" to iterate', 5, love.graphics.getHeight()-70)
+	love.graphics.print('3-9 to set sides', 5, love.graphics.getHeight()-120)
+	love.graphics.print('mousewheel to change angle', 5, love.graphics.getHeight()-170)
 end
